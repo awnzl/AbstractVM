df4e0f40fe966d8009c7c89086a424d0c248e7c8
Alexander Venzel <avenzel@e2r12p9.unit.ua>
2017-11-26 21:46:36 +0200

    make .cpp file for lexer

--- inc/AVMLexer.hpp
+++ inc/AVMLexer.hpp
@@ -5,41 +5,17 @@
 # include <regex>
 # include <stdexcept>
 
-//todo запилить .cpp файл
-
-
-
 class AVMLexer {
     std::regex ptrnINSRT;
 
-    AVMLexer () {
-        ptrnINSRT = std::string("push ((int8\\([-]?\\d+\\))|(int16\\([-]?\\d+\\))|(int32\\([-]?\\d+\\))|"\
-                                "(float\\([-]?\\d+\\.\\d+\\))|(double\\([-]?\\d+\\.\\d+\\)))|pop|dump|"\
-                                "assert ((int8\\([-]?\\d+\\))|(int16\\([-]?\\d+\\))|(int32\\([-]?\\d+\\))|"\
-                                "(float\\([-]?\\d+\\.\\d+\\))|(double\\([-]?\\d+\\.\\d+\\)))|add|sub|mul|"\
-                                "div|mod|print|exit|(\n+)");
-    }
-    // AVMLexer (const AVMLexer &);// {}
-    AVMLexer &operator=(AVMLexer &);// { return (*this); }
+    AVMLexer ();
+    AVMLexer &operator=(AVMLexer &);
 
 public:
     static AVMLexer &getLexer();
-
-    std::string *lexIt(std::string &s) {
-        std::string *res = NULL;
-
-        if (std::regex_match(s, ptrnINSRT))
-            res = new std::string(s);
-        else
-            throw (std::runtime_error("avm: invalid instruction"));
-
-        return (res);
-    }
+    std::string *lexIt(std::string &s);
 };
 
-AVMLexer &AVMLexer::getLexer() {
-    static AVMLexer lexer;
-    return (lexer);
-}
+static AVMLexer &getLexer();
 
 #endif
---
+++ src/AVMLeser.class.cpp
@@ -0,0 +1,26 @@
+// #include "AVMLexer.hpp"
+#include "../inc/AVMLexer.hpp"
+
+AVMLexer::AVMLexer () {
+    ptrnINSRT = std::string("push ((int8\\([-]?\\d+\\))|(int16\\([-]?\\d+\\))|(int32\\([-]?\\d+\\))|"\
+                            "(float\\([-]?\\d+\\.\\d+\\))|(double\\([-]?\\d+\\.\\d+\\)))|pop|dump|"\
+                            "assert ((int8\\([-]?\\d+\\))|(int16\\([-]?\\d+\\))|(int32\\([-]?\\d+\\))|"\
+                            "(float\\([-]?\\d+\\.\\d+\\))|(double\\([-]?\\d+\\.\\d+\\)))|add|sub|mul|"\
+                            "div|mod|print|exit|(\n+)");
+}
+
+AVMLexer &AVMLexer::getLexer() {
+    static AVMLexer lexer;
+    return (lexer);
+}
+
+std::string *AVMLexer::lexIt(std::string &s) {
+    std::string *res = NULL;
+
+    if (std::regex_match(s, ptrnINSRT))
+        res = new std::string(s);
+    else
+        throw (std::runtime_error("avm: invalid instruction"));
+
+    return (res);
+}
